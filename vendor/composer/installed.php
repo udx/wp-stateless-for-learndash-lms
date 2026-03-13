8f974d9b7b941bc6bfcc0f78d61dffbe3af01dc7
balexey88 <balexey88@gmail.com>
2026-03-13 18:40:31 +0200

    Prepare release

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'udx/wp-stateless-for-learndash-lms',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '17d1d03adcce507a0306c49a58bb4a739f79ccb3',
+        'reference' => '04f9b4bc00104bc43dd198b5857d30aa8a731db9',
         'type' => 'wordpress-plugin',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'udx/wp-stateless-for-learndash-lms' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '17d1d03adcce507a0306c49a58bb4a739f79ccb3',
+            'reference' => '04f9b4bc00104bc43dd198b5857d30aa8a731db9',
             'type' => 'wordpress-plugin',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
